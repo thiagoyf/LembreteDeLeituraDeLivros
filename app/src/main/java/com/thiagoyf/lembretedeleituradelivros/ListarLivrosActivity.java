c969831f8ddaf22f44a69cdd3450caf58b671a3f
thiagoyf <thiagoyudifukunaga@hotmail.com>
2016-12-10 22:08:53 -0300

    Criado CadastrarLivroActivity e ListarLivrosActivity
    Foi criado CadastrarLivroActivity junto com o seu xml
    activity_cadastrar_livro.xml e também foi criado ListarLivrosActivity
    junto com o seu xml activity_listar_livros.xml. Foi alterado o arquivo
    activity_cadastrar_livro.xml

---
+++ app/src/main/java/com/thiagoyf/lembretedeleituradelivros/ListarLivrosActivity.java
@@ -0,0 +1,13 @@
+package com.thiagoyf.lembretedeleituradelivros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListarLivrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_livros);
+    }
+}
--- app/src/main/java/com/thiagoyf/lembretedeleituradelivros/MainActivity.java
+++ app/src/main/java/com/thiagoyf/lembretedeleituradelivros/MainActivity.java
@@ -1,7 +1,9 @@
 package com.thiagoyf.lembretedeleituradelivros;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent();
+
+        switch (view.getId()) {
+            case R.id.main_btnCadastroDeLivros:
+                intent.setClass(this, CadastrarLivroActivity.class);
+                break;
+
+            case R.id.main_btnListaDeLivros:
+                intent.setClass(this, ListarLivrosActivity.class);
+                break;
+
+            case R.id.main_btnCadastroDeLembretes:
+                break;
+
+            case R.id.main_btnListaDeLembretes:
+                break;
+
+            default:
+                break;
+        }
+
+        startActivity(intent);
     }
 }
