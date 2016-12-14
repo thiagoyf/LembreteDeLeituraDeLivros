0c618aa58bd4d212ada6610983f1735e94aa2e48
thiagoyf <thiagoyudifukunaga@hotmail.com>
2016-12-14 14:13:08 -0300

    pickImage para Cadastro de Livro
    Foi criado métodos na classe CadastrarLivroActivity.java para que possa
    pegar imagem no diretório de imagem e salvar no cadastro de livros.
    Porém o cadastro da imagem do livro ainda não está feito!

--- app/src/main/java/com/thiagoyf/lembretedeleituradelivros/CadastrarLivroActivity.java
+++ app/src/main/java/com/thiagoyf/lembretedeleituradelivros/CadastrarLivroActivity.java
@@ -1,19 +1,34 @@
 package com.thiagoyf.lembretedeleituradelivros;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import dao.LivroDao;
 import model.Livro;
 
 public class CadastrarLivroActivity extends AppCompatActivity {
 
+    private static final int GALLERY_REQUEST = 1;
+
     private EditText edtLivroNome;
     private EditText edtLivroTotalPaginas;
+    private ImageView imgLivro;
 
     private LivroDao livroDao;
 
@@ -24,11 +39,12 @@ public class CadastrarLivroActivity extends AppCompatActivity {
 
         edtLivroNome = (EditText) findViewById(R.id.cadastrarLivro_edtNome);
         edtLivroTotalPaginas = (EditText) findViewById(R.id.cadastrarLivro_edtTotalDePaginas);
+        imgLivro = (ImageView) findViewById(R.id.cadastrarLivro_imgLivro);
 
         livroDao = new LivroDao(this);
     }
 
-    public void cadastrarLivro(View view){
+    public void cadastrarLivro(View view) {
         String nome = edtLivroNome.getText().toString();
         String totalPaginas = edtLivroTotalPaginas.getText().toString();
 
@@ -43,7 +59,7 @@ public class CadastrarLivroActivity extends AppCompatActivity {
             edtLivroTotalPaginas.setError(getString(R.string.campoObrigatório));
         }
 
-        if (validacao){
+        if (validacao) {
             Livro livro = new Livro();
             livro.setNome(nome);
             livro.setTotalPaginas(Integer.parseInt(totalPaginas));
@@ -51,7 +67,7 @@ public class CadastrarLivroActivity extends AppCompatActivity {
             if (livroDao.buscarLivroPorNome(nome) == null) {
                 long resultado = livroDao.salvarLivro(livro);
 
-                if (resultado != -1){
+                if (resultado != -1) {
                     Toast.makeText(this, getString(R.string.livroCadastradoSucesso),
                             Toast.LENGTH_LONG).show();
 
@@ -66,6 +82,52 @@ public class CadastrarLivroActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    public void pegaImagem(View view) {
+        escondeTeclado(this);
+
+        Intent intentPegaImagem = new Intent(Intent.ACTION_PICK);
+
+        File diretorioImagem = Environment.getExternalStoragePublicDirectory(Environment
+                .DIRECTORY_PICTURES);
+        String caminhoDiretorioImagem = diretorioImagem.getPath();
+
+        Uri uri = Uri.parse(caminhoDiretorioImagem);
+        intentPegaImagem.setDataAndType(uri, "image/*");
+
+        startActivityForResult(intentPegaImagem, GALLERY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == GALLERY_REQUEST && data != null) {
+            Uri imageUri = data.getData();
+
+            InputStream inputStream;
+
+            try {
+                inputStream = getContentResolver().openInputStream(imageUri);
+
+                Bitmap imagem = BitmapFactory.decodeStream(inputStream);
+                imgLivro.setImageBitmap(imagem);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Log.d("ImagePicker", getString(R.string.erroImagem));
+                Toast.makeText(this, getString(R.string.erroImagem), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public static void escondeTeclado(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService
+                (Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
     }
 
     @Override
